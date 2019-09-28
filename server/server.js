80cc428819e8a404338dc247bfddd22442dfe0e6
eyemotion <eyemotiongamestudio@gmail.com>
2019-09-28 14:30:10 +0400

    update

--- server/server.js
+++ server/server.js
@@ -1,34 +1,32 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const exercisesRouter = require('./routes/exercises')
-const userRouter = require('./routes/users')
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
-const uri = process.env.URI
-mongoose.set('useUnifiedTopology', true);
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true })
+const uri = process.env.URI;
+mongoose.set("useUnifiedTopology", true);
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 
-const connection = mongoose.connection
+const connection = mongoose.connection;
 
-connection.once('open', () => {
-    console.log("MongoDB database connection established successfully ")
-})
+connection.once("open", () => {
+    console.log("MongoDB database connection established successfully ");
+});
 
 // Middle ware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use('/exercises', exercisesRouter)
-app.use('/users', userRouter)
-
-
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`)
-})
+    console.log(`Server is running on port: ${port}`);
+});
